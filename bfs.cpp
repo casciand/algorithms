4352e887591fdd7ca15f2d2c799898dd03a3b3ef
Andrew Cascio <andrewccascio@gmail.com>
2022-06-14 20:47:05 -0400

    finish dfs and bfs

--- bfs.cpp
+++ bfs.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <queue>
+#include "Graph.h"
 
+void Graph::bfs(int v) {
+    std::queue<int> q;
+    q.push(v);
 
-void bfs() {
-    return;
+    while (!q.empty()) {
+        // visit node
+        v = q.front();
+        std::cout << v << " ";
+        visited[v] = true;
+        q.pop();
+
+        // add neighbors to queue
+        for (int w : adj[v]) {
+            if (!visited[w]) {
+                q.push(w);
+            }
+        }
+    }
 }
 
 int main() {
+    Graph g;
+    g.bfs(1); // O(V + E)
+
     return 0;
 }
--- dfs.cpp
+++ dfs.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include "Graph.h"
 
-// dfs, bfs, strongly connected components, articulation points, prims, kruskals, prims w/ heap, kruskals w/ heap
-// general: greedy, dynamic programming, sorting
+void Graph::dfs(int v) {
+    if (!visited[v]) {
+        // visit node
+        visited[v] = true;
+        std::cout << v << " ";
+
+        // recurse for each neighbor
+        for (int w : adj[v]) {
+            dfs(w);
+        }
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Graph g;
+    g.dfs(1); // O(V + E)
+
     return 0;
 }
